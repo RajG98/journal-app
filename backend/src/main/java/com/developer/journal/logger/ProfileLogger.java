640c6fcca0cb325a25f0cc23e348f8a83d3ef8ed
RajG98 <rajarshig007@gmail.com>
2025-07-26 23:35:24 +0530

    update

--- backend/src/main/java/com/developer/journal/logger/ProfileLogger.java
+++ backend/src/main/java/com/developer/journal/logger/ProfileLogger.java
@@ -14,15 +14,15 @@ public class ProfileLogger implements CommandLineRunner {
     private String profile;
 
 
-//    @Value("${security.jwt.secret-key}")
-//    private String key;
-//
-//    @Override
-//    public void run(String... args) {
-//        log.info("ACTIVE SPRING PROFILE: {} with jwt key: {}", profile,key);
-//    }
+    @Value("${security.jwt.secret-key}")
+    private String key;
+
     @Override
     public void run(String... args) {
-        log.info("ACTIVE SPRING PROFILE: {}", profile);
+        log.info("ACTIVE SPRING PROFILE: {} with jwt key: {}", profile,key);
     }
+//    @Override
+//    public void run(String... args) {
+//        log.info("ACTIVE SPRING PROFILE: {}", profile);
+//    }
 }
